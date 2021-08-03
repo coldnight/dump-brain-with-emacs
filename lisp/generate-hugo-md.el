ac5567550c8935a211239699339ea0fad212e01c
wanghui <wh_linux@126.com>
2021-08-03 16:09:34 +0800

    copy README.org to notes

--- lisp/generate-hugo-md.el
+++ lisp/generate-hugo-md.el
@@ -1,7 +1,11 @@
 
 (defun generate-readme-to-hugo()
   "Generate README to hugo."
-  (with-temp-buffer
-    (find-file (expand-file-name "README.org" "~"))
-    (org-hugo-export-as-md))
+  (let ((exists-fn (expand-file-name "README.org" "~"))
+        (dest-fn (expand-file-name "notes/content-org/dump-brain-with-emacs.org" "~")))
+    (copy-file exists-fn dest-fn)
+    (with-temp-buffer
+      (find-file dest-fn)
+      (org-mode)
+      (org-hugo-export-as-md)))
   (message "done!"))
