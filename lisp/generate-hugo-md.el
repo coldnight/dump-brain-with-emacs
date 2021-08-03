ec22fed25c3cc61a444919300407f54b9d435677
wanghui <wh_linux@126.com>
2021-08-03 16:47:36 +0800

    fix typo

--- lisp/generate-hugo-md.el
+++ lisp/generate-hugo-md.el
@@ -2,10 +2,13 @@
 (defun generate-readme-to-hugo()
   "Generate README to hugo."
   (let ((exists-fn (expand-file-name "README.org" "~"))
-        (dest-fn (expand-file-name "notes/content-org/dump-brain-with-emacs.org" "~")))
+        (dest-fn (expand-file-name "notes/content-org/dump-brain-with-emacs.org" "~"))
+        (source-images (expand-file-name "images" "~"))
+        (dest-images (expand-file-name "notes/content-org/images" "~")))
     (copy-file exists-fn dest-fn)
+    (copy-directory source-images dest-images t t)
     (with-temp-buffer
       (find-file dest-fn)
       (org-mode)
-      (org-hugo-export-as-md)))
+      (org-hugo-export-to-md)))
   (message "done!"))
